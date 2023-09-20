d24e1fe26ae2487daa37b62fbe3afb9ec6d2b991
Nicolas Magalhaes <nicolas.magalhaes.clear@gmail.com>
2023-09-20 10:44:46 -0300

    update: pageHeader, TodoForm and TodoList added

--- src/todo/todo.jsx
+++ src/todo/todo.jsx
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import PageHeader from '../template/pageHeader';
+import TodoForm from './todoForm';
+import TodoList from './todoList';
 
 export default class Todo extends Component {
     render() {
         return (
             <div>
-                <h1>Teste</h1>
+                <PageHeader name='todo' small='app'/>
+                <TodoForm/>
+                <TodoList/>
             </div >
         )
     }
