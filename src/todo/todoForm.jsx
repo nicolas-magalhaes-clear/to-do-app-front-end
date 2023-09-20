b28d43f95a5df1a778eb00aed058d45819152b8a
Nicolas Magalhaes <nicolas.magalhaes.clear@gmail.com>
2023-09-20 10:44:58 -0300

    added: todoForm.jsx

---
+++ src/todo/todoForm.jsx
@@ -0,0 +1,20 @@
+import React from "react"
+import Grid from "../template/grid"
+import IconButton from "../template/iconButton"
+
+
+export default props => {
+    return (
+        <div role="form" className="todoForm">
+            <Grid cols='12 9 10'>
+                <input id="description" className="form-control" placeholder="Adicione uma tarefa" />
+
+            </Grid>
+            <Grid cols='12 3 2'>
+
+                <IconButton style='primary' icon='plus'/>
+
+            </Grid>
+        </div>
+    )
+}
