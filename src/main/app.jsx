3330aa4a24b7f5309c899ba46da8ca0449f74a91
Nicolas Magalhaes <nicolas.magalhaes.clear@gmail.com>
2023-09-20 10:05:49 -0300

    update: Menu and routes components added

--- src/main/app.jsx
+++ src/main/app.jsx
@@ -1,15 +1,14 @@
-
-import 'modules/bootstrap/dist/css/bootstrap.min.css';
-import 'modules/font-awesome/css/font-awesome.min.css';
+import 'modules/bootstrap/dist/css/bootstrap.min.css'
+import 'modules/font-awesome/css/font-awesome.min.css'
 
 
 import React from 'react'
+import Menu from '../template/menu'
+import Routes from './routes'
 
-
-export default function App (props){
-    return (
-        <div className="container">
-            <h1>Teste</h1>
-        </div>
-    )
-}
+export default props => (
+    <div className='container'>
+        <Menu />
+        <Routes />
+    </div>
+)
